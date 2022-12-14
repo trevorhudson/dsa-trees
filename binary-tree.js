eb93e7c259618408f2a331e7cbeac2ee1dfccb8a
trevorhudson <trevor.c.hudson@gmail.com>
2022-12-14 11:21:21 -0800

    finished sumValues()

---
+++ binary-tree.js
@@ -0,0 +1,64 @@
+"use strict";
+
+/** BinaryTreeNode: node for a general tree. */
+
+class BinaryTreeNode {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+
+  /** minDepth(): return the minimum depth from the invoking node -- that is,
+   * the length of the shortest path from the invoking node to a leaf. */
+  minDepth() {
+
+  }
+
+  /** maxDepth(): return the maximum depth from the invoking node -- that is,
+   * the length of the longest path from the invoking node to a leaf. */
+  maxDepth() {
+
+  }
+}
+
+class BinaryTree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  /** minDepth(): return the minimum depth of the tree -- that is,
+   * the length of the shortest path from the root to a leaf. */
+
+  // this is a stack or recursion problem; we'll use recursion
+
+  minDepth() {
+
+  }
+
+  /** maxDepth(): return the maximum depth of the tree -- that is,
+   * the length of the longest path from the root to a leaf. */
+
+  // this is a stack or recursion problem; we'll use recursion
+
+  maxDepth() {
+
+  }
+
+  /** nextLarger(lowerBound): return the smallest value in the tree
+   * which is larger than lowerBound. Return null if no such value exists. */
+
+  nextLarger(lowerBound) {
+
+  }
+
+  /** Further study!
+   * areCousins(node1, node2): determine whether two nodes are cousins
+   * (i.e. are at the same level but have different parents. ) */
+
+  areCousins(node1, node2) {
+
+  }
+}
+
+module.exports = { BinaryTree, BinaryTreeNode };
---
+++ tree.js
@@ -0,0 +1,65 @@
+"use strict";
+
+/** TreeNode: node for a general tree. */
+
+class TreeNode {
+  constructor(val, children = []) {
+    this.val = val;
+    this.children = children;
+  }
+
+  /** sumValues(): add up all values of invoking node and its children.
+   * Returns sum as an integer. */
+  sumValues(acc = 0) {
+    if (this.children.length === 0) return this.val;
+
+    for (let child of this.children) {
+      acc += child.sumValues();
+
+    }
+
+    return acc += this.val;
+  }
+
+
+
+  /** countEvens(): starting from the invoking node and moving through its
+   * children, count how many nodes have even values. Returns that count as
+   * an integer. */
+  countEvens() {
+
+  }
+
+  /** numGreater(lowerBound): starting from the invoking node and moving through
+   * its children, return a count of the number of nodes whose value is greater
+   * than lowerBound. */
+  numGreater(lowerBound) {
+
+  }
+}
+
+class Tree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  /** sumValues(): add up all values in the tree. */
+  sumValues() {
+    if (this.root == null) return 0;
+    return this.root.sumValues();
+
+  }
+
+  /** countEvens(): count all nodes in the tree that have even values. */
+  countEvens() {
+
+  }
+
+  /** numGreater(lowerBound): return a count of the number of nodes
+   * whose value is greater than lowerBound. */
+  numGreater(lowerBound) {
+
+  }
+}
+
+module.exports = { Tree, TreeNode };
